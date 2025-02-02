39eeffacfbffbad9ca982a16748dfca0365ae256
Dominic Kronig <dominic.kronig@students.unibe.ch>
2025-02-02 17:28:06 +0100

    super messy but works somewhat, transition effect

---
+++ fade_in.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Select the main container
+    const mainContainer = document.querySelector('.main_container');
+    
+    // Add the fade-in class to it once the content has loaded
+    mainContainer.classList.add('fade-in');
+});
